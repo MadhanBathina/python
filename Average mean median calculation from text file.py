02efaccbb661824871306d82de359c02d7f5a5e2
Madhan Bathina <madhanbathina22770@gmail.com>
2021-10-09 16:33:33 +0530

    succeess

--- Average mean median calculation from text file.py
+++ Average mean median calculation from text file.py
@@ -1,61 +1,33 @@
 
 #open and read the text file data contains marks list
-from math import floor
 
 #Calculate the Average, mean, median
-f=open("marks.txt",'r')
-l=f.readline() # read line and store in l varaiable
-k=0
-c=0
-m=[]
-#s=len(l)
-#print('find',l.find('0'))
-#print('type',type(l))
-#print('index',l[0])
-#print('length-s',s)
-#l1=False
-for i in l.split(','):
-    #print(i)
-    #print(n)
-    m=m+[i]
-    k+=float(i)
-    #print(k)
-    c+=1
-    #print(k)
-if c%2==0:
-    median=((c/2)+1)
-    g=median-1
-    #print('g',g)
-    b=floor(g)
-    print('median',m[b])
+from math import floor
+file  = open("marks.txt",'r')
+line  = file.readline() # read line and store in l varaiable
+total = 0
+count = 0
+list_of_numbers = []
+arranged_list = []
+indexnum=0
+
+for num in line.split(','):
+    
+    list_of_numbers = list_of_numbers + [int(num)]
+
+    total+=float(num)
+
+    count+=1
+    
+list_of_numbers.sort()
+
+
+if count%2 == 0:
+    medianindex=floor(count/2)
+    print('median',list_of_numbers[medianindex])
 else:
-    median=((c+1)/2)
-    g=median-1
-    #print('g',g)
-    b=floor(g)
-    print('median',m[b])
+    medianindex=floor(((count+1)/2)-1)
+    print('median',list_of_numbers[medianindex])
 
 #print(k)
-print('Mean',k/c)
-#print('c',c)
-#print(m)
-#print(len(m))
-#print(g)
-
-    #j=l[i]
-    #print('j',j)
-    #if j==',' :
-       # print('j2',j)
-   # else:
-      #  print('l')
-    #if l.isalphacii:
-     #   l1=True
-    #  print(l1)
-#for i in range(0,s):
-    #k=+1
-    #print(l[i])
-
-        
-
-#with open("marks.txt") as f:
- #   print(f.readlines())
+print('Mean',total/count)
