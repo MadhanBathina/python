6e415c6c96b2dd82d909e0442275aff138fc4528
Madhan Bathina <madhanbathina22770@gmail.com>
2021-10-12 22:02:15 +0530

    hacker rank code solve

--- plus minus array.py
+++ plus minus array.py
@@ -1,18 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
 def plusMinus(arr):
     # Write your code here
-    plus_num=0
-    minus_num=0
-    zero_count=0
-    for i in range(0,len(arr)):
-        if arr[i]>0:
-            plus_num+=1
-        elif arr[i]<0:
-            minus_num+=1
-        else:
-            zero_count+=1
-    print('proportion of positive values',plus_num/n)
-    print('proportion of negative values',minus_num/n)
-    print('proportion of zeros',zero_count/n)
+    plus_count  = 0
+    minus_count = 0
+    zero_count  = 0
+    for i in range ( 0 , len(arr) ) :
+        if   arr[i] > 0 :
+            plus_count  = plus_count  + 1
+        elif arr[i] < 0 :
+            minus_count = minus_count + 1
+        else :
+            zero_count = zero_count + 1
+
+    print("{:.6f}".format(minus_count/n))
+    print("{:.6f}".format(zero_count/n))
+    print("{:.6f}".format(plus_count/n))
+            
+
 if __name__ == '__main__':
     n = int(input().strip())
 
